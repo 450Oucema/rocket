208dac412668ece591ac44d65fabc6cdb6521e39
450Oucema <450oucema@gmail.com>
2020-01-27 00:02:40 +0100

    Interface revue et génération des images

---
+++ Jeux.js
@@ -0,0 +1,25 @@
+class Jeux {
+    constructor() {
+        this.nom = nom;
+        this.chemin = chemin;
+        this.cover = cover;
+    }
+    set nom(val) {
+        this.nom = val;
+    }
+    get nom() {
+        return this.nom;
+    }
+    set chemin(val) {
+        this.chemin = val;
+    }
+    get chemin() {
+        return this.chemin;
+    }
+    set cover(val) {
+        this.cover = val;
+    }
+    get cover() {
+        return this.cover;
+    }
+}
--- renderer.js
+++ renderer.js
@@ -4,6 +4,7 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+/**https://rawgthedocs.orels.sh/api/games/#get */
 
 // Appel des librairies utilisées
 
@@ -13,6 +14,11 @@ require('bootstrap');
 require('uikit');
 const Shell = require('node-powershell');
 var mysql = require('mysql');
+const Rawger = require('rawger');
+const slash = require('slash');
+var download = require('download-file')
+
+var repert = "";
 
 // Connexion à la BDD
 var connection = mysql.createConnection({
@@ -23,6 +29,10 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+const ps = new Shell({
+    executionPolicy: 'Bypass',
+    noProfile: true
+});
 
 //Affichage dynamique
 
@@ -31,7 +41,7 @@ connection.query('SELECT * FROM `jeu`', function(error, jeux, fields) {
 
     jeux.forEach(jeu => {
         console.log(jeu.nom);
-        $('.uk-thumbnav').append("<li id='" + jeu.id + "' ><a onclick='SelectionJeu(" + jeu.id + ");' class='thumbjeux'><img style='max-height: 115px; max-width: 200px;' src='" + jeu.cover + "'></a></li>");
+        $('.uk-thumbnav').append("<li id='" + jeu.id + "' ><a onclick='SelectionJeu(" + jeu.id + ");' class='thumbjeux'><img class='imgcard' src='./assets/cover/" + jeu.cover + "'> <div class='uk-position-center uk-overlay uk-overlay-default'>" + jeu.nom + "</div></a></li>");
     });
 
 });
@@ -48,10 +58,7 @@ function SelectionJeu(id) {
 }
 
 
-const ps = new Shell({
-    executionPolicy: 'Bypass',
-    noProfile: true
-});
+
 
 function exeJeux(chemin) {
     //var command = String.raw `Start-Process -FilePath "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe" -Wait`.split('\\').join('/');
@@ -66,3 +73,100 @@ function exeJeux(chemin) {
             console.log(err);
         });
 }
+
+
+
+
+
+async function RechRAWGER(nom) {
+    //chemin = String.raw `` + chemin + ``.split('\\').join('/');
+    repertoire = repert;
+    alert("chemin2: " + repertoire);
+    const { games } = await Rawger({});
+    const results = (await games.search(nom)).get();
+    console.log(results);
+    var count = Object.keys(results).length;
+    if (count > 0) {
+        $('.proposition').html('<div class="input-group"><input id="titre_prop" type="text" class="form-control" value="' + results[0].name + '"><div class="input-group-append"><button class="uk-button uk-button-primary" id="validjeux"  type="button"><i class="fas fa-check"></i></button><button id="nouvrecherche" class="uk-button uk-button-danger" type="button"><i class="fas fa-times"></i></button></div></div><hr><ul class="uk-thumbnav"><li class="uk-active"><img src="' + results[0].image + '" alt=""></li></ul></div>');
+
+
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+async function nouvRecherche() {
+    var nom = $('#titre_prop').val();
+    const { games } = await Rawger({});
+    const results = (await games.search(nom)).get();
+    var count = Object.keys(results).length;
+    if (count > 0) {
+        $('.proposition').html('<div class="input-group"><input id="titre_prop" type="text" class="form-control" value="' + results[0].name + '" ><div class="input-group-append"><button id="validjeux" class="uk-button uk-button-primary" type="button"><i class="fas fa-check"></i></button><button id="nouvrecherche" class="uk-button uk-button-danger" type="button"><i class="fas fa-times"></i></button></div></div><hr><ul class="uk-thumbnav"><li class="uk-active"><img src="' + results[0].image + '" alt=""></li></ul></div>');
+    }
+}
+
+
+async function validJeux(nom) {
+
+    const { games } = await Rawger({});
+    const results = (await games.search(nom)).get();
+    var opt = {
+        directory: "./assets/cover/",
+        filename: results[0].slug + ".jpg"
+    }
+    download(results[0].image, opt, function(err) {
+        if (err) throw err
+        console.log('çamarche');
+    })
+
+
+
+    ajoutJeux(results[0].name, results[0].slug + ".jpg");
+
+}
+
+function ajoutJeux(nom, image) {
+
+    connection.query("INSERT INTO `jeu` (`nom`, `chemin`, `cover`) VALUES ('" + nom + "', '" + repert + "', '" + image + "')", function(error, resultat, fields) {
+        if (error) throw error;
+        //location.reload(true);
+    });
+
+}
+
+
+/** JQUERY */
+$(document).on('click', '#validjeux', function() {
+    nom = $('#titre_prop').val();
+    chemin = $('#rp').val();
+    validJeux(nom);
+});
+
+$(document).on('click', '#nouvrecherche', function() {
+    console.log(repert);
+    nouvRecherche();
+});
+
+$('.uk-card-default').hover(function() {
+    $('.uk-card-body').css("display", "inline");
+});
+
+
+/** DESIGN JQUERY */
+"use strict";
+
+var fullHeight = function() {
+
+    $('.js-fullheight').css('height', $(window).height());
+    $(window).resize(function() {
+        $('.js-fullheight').css('height', $(window).height());
+    });
+
+};
+fullHeight();
+
+$('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
+});
